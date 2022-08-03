40603edef22d7303faa49b4c4512e095ef4fe5fe
Douglas Marodin <douglasmarodin@gmail.com>
2022-08-03 01:50:20 -0300

    Add storybook

---
+++ src/lib/components/button.stories.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+	import '@skinned-ui/style';
+	import Button from './button.svelte';
+</script>
+
+<Meta title="skinned-ui/Button" component={Button} />
+
+<Story name="Default">
+	<Button>Save</Button>
+</Story>
