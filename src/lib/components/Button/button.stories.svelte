308d964faa66c473b9233fb82d3bb469446eec86
Douglas Marodin <douglasmarodin@gmail.com>
2022-08-09 01:26:12 -0300

    Add slightly advanced button styling

--- src/lib/components/Button/button.stories.svelte
+++ src/lib/components/Button/button.stories.svelte
@@ -2,18 +2,36 @@
 	import Button from './Button.svelte';
 	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
 	import '@skinned-ui/style';
+
+	function getClass(args) {
+		const classes = [...(args.outline ? ['outline'] : [])];
+		return classes.join(' ');
+	}
 </script>
 
 <Meta
 	title="Components/Button"
 	component={Button}
 	argTypes={{
-		label: { control: 'text' }
+		label: { control: 'text' },
+		outline: { control: 'boolean' }
 	}}
 />
 
-<Template let:args>
+<Template id="all" let:args>
 	<Button>{args.label}</Button>
+	<Button class={`blue ${getClass(args)}`}>{args.label}</Button>
+	<Button class={`purple ${getClass(args)}`}>{args.label}</Button>
+	<Button class={`green ${getClass(args)}`}>{args.label}</Button>
+	<Button class={`red ${getClass(args)}`}>{args.label}</Button>
 </Template>
 
-<Story name="Default" args={{ label: 'Save' }} />
+<Template id="outline" let:args>
+	<Button class={`blue ${getClass(args)}`}>{args.label}</Button>
+	<Button class={`purple ${getClass(args)}`}>{args.label}</Button>
+	<Button class={`green ${getClass(args)}`}>{args.label}</Button>
+	<Button class={`red ${getClass(args)}`}>{args.label}</Button>
+</Template>
+
+<Story name="Default" template="all" args={{ label: 'Save' }} />
+<Story name="Outline" template="outline" args={{ label: 'Save', outline: true }} />
