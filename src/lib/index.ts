9837ca95d2b77ae2c9efcc05a75484ccd06743f8
Douglas Marodin <douglasmarodin@gmail.com>
2022-08-14 22:30:11 -0300

    Add title component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './components/Button/Button.svelte';
 export { default as Input } from './components/Input/Input.svelte';
 export { default as Card } from './components/Card/Card.svelte';
+export { default as Title } from './components/Title/Title.svelte';
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,27 @@
 <script lang="ts">
 	import '@skinned-ui/style';
 	import '../style.css';
-	import { Input, Card } from '$lib';
+	import { Input, Card, Title } from '$lib';
 </script>
 
-<main class="">
+<main>
 	<Card>
-		<Input title="Username" value="John" />
-		<div style="width: 10px;display:inline-block;" />
-		<Input title="Username" value="John" error="Invalid username" />
-		<div style="width: 10px;display:inline-block;" />
-		<Input title="Username" value="John" horizontal />
-		<div style="width: 10px;display:inline-block;" />
-		<Input title="Username" value="John" horizontal error="Invalid username" />
+		<Title>Vertical Input</Title>
+		<div class="flex gap-4">
+			<Input title="Username" value="John" />
+			<Input title="Username" value="John" error="Invalid username" />
+		</div>
+	</Card>
+	<Card class=" mt-4">
+		<Title>Horizontal Input</Title>
+		<div class="flex gap-4">
+			<Input title="Username" value="John" horizontal />
+			<Input title="Username" value="John" horizontal error="Invalid username" />
+		</div>
 	</Card>
 	<Card class="mt-4">
-		<Input title="Username" value="John" horizontal error="Invalid username" />
-		<div style="width: 10px;display:inline-block;" />
+		<Title>Required Input</Title>
+		<Input title="Username" value="John" error="Invalid username" />
 	</Card>
 </main>
 
