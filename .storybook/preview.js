6052c7e36a047221a69c2b8b401cd975a5c62db9
Douglas Marodin <douglasmarodin@gmail.com>
2022-08-15 22:08:48 -0300

    Add card and tabgroup story

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,13 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/
+		}
+	},
+	backgrounds: {
+		default: 'dark',
+		values: [{ name: 'dark', value: '#1B1D25' }]
+	}
+};
---
+++ src/lib/components/Card/Card.stories.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import Card from './Card.svelte';
+	import '@skinned-ui/style';
+</script>
+
+<Meta title="Components/Card" component={Card} argTypes={{}} />
+
+<Template let:args>
+	<Card {...args} />
+</Template>
+
+<Story name="Default" args={{}} />
